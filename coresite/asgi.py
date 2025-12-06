4b51d167a168748ffd0085f04d714009c4578753
abdussamud <abdulsamad.aaa123@gmail.com>
2025-12-06 12:22:17 +0500

    change asgi file

--- coresite/asgi.py
+++ coresite/asgi.py
@@ -1,18 +1,18 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from apps.dashboard.middleware import TokenAuthMiddleware
-from apps.dashboard.routing import websocket_urlpatterns
+# from apps.dashboard.middleware import TokenAuthMiddleware
+# from apps.dashboard.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coresite.settings')
 
-app = get_asgi_application()
+application = get_asgi_application()
 
 
-application = ProtocolTypeRouter({
-    "http": app,
-    "websocket": TokenAuthMiddleware(
-        URLRouter(websocket_urlpatterns)
-    ),
-})
+# application = ProtocolTypeRouter({
+#     "http": app,
+#     "websocket": TokenAuthMiddleware(
+#         URLRouter(websocket_urlpatterns)
+#     ),
+# })
