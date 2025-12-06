158387539923be6ce54a5e547465ddcd6def7e4e
abdussamud <abdulsamad.aaa123@gmail.com>
2025-12-06 12:03:11 +0500

    remove channel daphni

--- apps/dashboard/consumers.py
+++ apps/dashboard/consumers.py
@@ -1,17 +1,17 @@
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
-
-
-class OrderStatusConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        await self.channel_layer.group_add("orders", self.channel_name)
-
-    async def disconnect(self, code):
-        await self.channel_layer.group_discard("orders", self.channel_name)
-
-    async def order_update(self, event):
-        message = event.get("message")
-        await self.send_json(message)
-
-    async def receive_json(self, content, **kwargs):
-        pass
+# from channels.generic.websocket import AsyncJsonWebsocketConsumer
+#
+#
+# class OrderStatusConsumer(AsyncJsonWebsocketConsumer):
+#     async def connect(self):
+#         await self.accept()
+#         await self.channel_layer.group_add("orders", self.channel_name)
+#
+#     async def disconnect(self, code):
+#         await self.channel_layer.group_discard("orders", self.channel_name)
+#
+#     async def order_update(self, event):
+#         message = event.get("message")
+#         await self.send_json(message)
+#
+#     async def receive_json(self, content, **kwargs):
+#         pass
